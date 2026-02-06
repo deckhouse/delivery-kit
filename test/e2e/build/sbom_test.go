0b5a54d7a1638f8f3c71c28b8bc2fe5cc1a594fb
Radmir Khurum <radmir.khurum@flant.com>
2026-02-06 14:33:38 +0700

    chore(sbom): update test message check
    Signed-off-by: Radmir Khurum <radmir.khurum@flant.com>

--- test/e2e/build/sbom_test.go
+++ test/e2e/build/sbom_test.go
@@ -183,7 +183,7 @@ var _ = Describe("Simple build", Label("e2e", "build", "sbom", "simple"), func()
 				out, err := werfProject.BuildWithErr(ctx, nil)
 
 				Expect(err).To(HaveOccurred(), "build should fail when base image SBOM is not found")
-				Expect(out).To(ContainSubstring("not found in container registry"))
+				Expect(out).To(ContainSubstring("unable to resolve SBOM name for image"))
 			},
 			Entry("stapel with local repo using Vanilla Docker", baseImageSbomTestOptions{
 				setupEnvOptions: setupEnvOptions{
